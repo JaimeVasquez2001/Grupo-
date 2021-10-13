81fc9d7aef4a061158a7abf2cd9a6128962e8e82
JaimeVasquez2001 <92454528+JaimeVasquez2001@users.noreply.github.com>
2021-10-13 10:34:52 -0500

    Add files via upload

---
+++ main4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main() {
+int n;
+cuot<<"INGRESE UN NUMERO:";
+cin>>n;
+cuot<<"SU INGRESE UN NUMERO:"<<n;
+	return 0;
+}
